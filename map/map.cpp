cd3670e57601419849cc9ff339ae606290fbcdc2
chiptime <amor95_8@hotmail.com>
2018-06-19 23:15:47 +0200

    pong complete

--- map/map.cpp
+++ map/map.cpp
@@ -9,11 +9,8 @@
 void dibujar_Marco(){//char matriz[fila][columna]){
     for(int f=0; f<fila;f++) {
         for(int c=0;c<columna;c++) {
-            if(((f) == minf) || ((f) == maxf)){
-                if( ((f==minf)&&(c==minc)) || ((f==minf)&&(c==maxc)) || ((f==maxf)&&(c==minc)) || ((f==maxf)&&(c==maxc)) )
-                    printw("█");
-                else printw("█");
-            }
+            if(((f) == minf) || ((f) == maxf))
+                printw("█");
             else
             if( (c<(minc+1)) ||(c>(maxc-1)) )
                 printw("█");
